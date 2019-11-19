f8eb2368fe4798de7b0d8122466f9a82957ed917
StephenTHendrix <stephenthendrix@gmail.com>
2019-11-19 11:07:19 -0600

    search save

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,36 +1,67 @@
-import React from "react";
-import ResultsContainer from "../components/Results";
+import React, { Component } from "react";
+// import DeleteBtn from "../components/DeleteBtn";
+import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
+// import { Link } from "react-router-dom";
+import { Container } from "../components/Grid";
+import { List, ListItem } from "../components/List";
+import { Input, TextArea, FormBtn } from "../components/Form";
 
-class Saved extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            savedBooks: []
-        }
-    }
-    
+class Saved extends Component {
+  state = {
+    books: []
+  };
 
-    componentDidMount() {
-        API.getBooks().then(
-            (response) => {
-                this.setState({ savedBooks: response.data });
-            }
-        ).catch(
-            (err) => {
-                console.log(err);
-            }
-        );
-    }
+  componentDidMount() {
+    this.loadBooks();
+  }
 
-    render() {
-        console.log(this.state.savedBooks);
-        return (
-            <main>
-                <ResultsContainer savedBooks={this.state.savedBooks} path={this.props.match.path} />
-            </main>
-        );
-    }
+  
+  loadBooks = () => {
+    API.getBooks()
+      .then(res =>
+        this.setState({
+          books: res.data,
+        })
+      )
+      .catch(err => console.log(err));
+  };
+
+  // deleteBook = id => {
+  //   API.deleteBook(id)
+  //     .then(res => this.loadBooks())
+  //     .catch(err => console.log(err));
+  // };
+
+  render() {
+    return (
+      <Container fluid>
+        <Jumbotron>
+          <h1>Books On My List</h1>
+        </Jumbotron>
+        {this.state.books.length ? (
+          <List>
+            {this.state.books.map(book => (
+              <ListItem key={book._id}>
+                
+                  <img src={book.image} alt = ""/>
+                  <strong>
+                    <a href={book.link}>
+                      {book.title} by {book.authors}
+                    </a>
+                  </strong>
+                  <p>{book.description}</p>
+               
+                
+              </ListItem>
+            ))}
+          </List>
+        ) : (
+          <h3>No Results to Display</h3>
+        )}
+      </Container>
+    );
+  }
 }
 
 export default Saved;
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,44 +1,112 @@
-import React from "react";
-import SearchForm from "../components/SearchForm";
-import ResultsContainer from "../components/Results";
+import React, { Component } from "react";
 import API from "../utils/API";
+import axios from "axios";
+// import { Link } from "react-router-dom";
+import { Container } from "../components/Grid";
+import { List, ListItem } from "../components/List";
+import { Input, FormBtn } from "../components/Form";
 
-class Search extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            bookInput: "",
-            bookData: []
-        }
-        this.handleSearchClick = this.handleSearchClick.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+
+class Search extends Component {
+    state = {
+        books: []
     }
 
-    handleChange(e) {
-        e.preventDefault();
-        this.setState({ bookInput: e.target.value })
+
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
+
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+
+        axios.get("https://www.googleapis.com/books/v1/volumes?q=" + this.state.title)
+            .then(response => {
+
+                this.setState({
+                    books: response.data.items.map(item => {
+                        // console.log(item.volumeInfo)
+                        return {
+                            title: item.volumeInfo.title,
+                            authors: item.volumeInfo.authors,
+                            description: item.volumeInfo.description,
+                            image: item.volumeInfo.imageLinks.thumbnail,
+                            link: item.volumeInfo.infoLink
+                        }
+
+                    })
+                })
+                console.log(this.state.books)
+            })
     }
 
-    handleSearchClick(e) {
-        e.preventDefault();
-        API.searchBooks(this.state.bookInput)
-            .then(
-                (response) => {
-                    this.setState({ bookData: response.data });
-                    this.setState({ bookInput: "" });
-                }
-            );
-    }
+    handleSaveBook = data => {
+        // event.preventDefault();
+         console.log(data);
+          API.addBookToDB({
+            title: data.title,
+            authors: data.authors,
+            description: data.description,
+            image: data.image,
+            link: data.link
+          })
+            // .then(res => this.loadBooks())
+            .catch(err => console.log(err));
+        
+      };
+
 
     render() {
-        return (
-            <main>
-                <SearchForm handleChange={this.handleChange} handleSearchClick={this.handleSearchClick} />
-                {(this.state.bookData.length > 0) ?
-                    <ResultsContainer bookData={this.state.bookData} path={this.props.match.path} /> : null
-                }
-            </main>
-        );
+        return (<Container fluid>
+          
+
+
+            <form>
+                <Input
+                    value={this.state.title}
+                    onChange={this.handleInputChange}
+                    name="title"
+                    placeholder="Title (required)"
+                />
+
+                <FormBtn
+                    onClick={this.handleFormSubmit}
+                >
+                    Search
+                  </FormBtn>
+            </form>
+
+            {this.state.books.length ? (
+                <List>
+                    {this.state.books.map(book => (
+                        <div>
+
+                            <ListItem key={book._id}>
+                                <FormBtn onClick={() => this.handleSaveBook(book)}>
+                                    Save
+                                </FormBtn>
+                                <FormBtn>
+                                    Delete
+                                </FormBtn>
+
+                                <img src={book.image} alt = ""/>
+                                <strong><a href={book.link}>{book.title} by {book.authors}</a></strong>
+                                <p>{book.description}</p>
+                            </ListItem>
+
+                        </div>
+                    ))}
+                </List>
+            ) : (
+                    <h3>No Results to Display</h3>
+                )}
+        </Container>)
     }
 }
 
